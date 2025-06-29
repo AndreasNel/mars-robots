c346af36fdaacdce1b366e12cb8f9a1c7ee65528
Andreas Nel <nel.andreas1@gmail.com>
2025-06-29 21:18:51 +0000

    Add ability to read input file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log("This is the main entry point of the Mars Robots application.");
+/* eslint-disable no-console */
+import { pathToFileURL } from "url";
+import Robot from "./models/robot";
+import World from "./models/world";
+import processCommandString from "./services/process-command-string";
+
+function processRobots(input) {
+  const [boundingBox, ...rows] = input.split("\n");
+  const filteredRows = rows.filter((row) => row.trim() !== "");
+
+  const world = World.createFromBoundingBox(boundingBox);
+  console.log({ world });
+
+  // Iterate through the rows 2 at a time. The first row is the robot position, the second row is the commands.
+  for (let i = 0; i < filteredRows.length; i += 2) {
+    const position = filteredRows[i].split(" ");
+    const commandString = filteredRows[i + 1].trim();
+
+    const robot = Robot.createFromPosition(position);
+    console.log({ robot });
+    const result = processCommandString(robot, commandString, world);
+    console.log({ result });
+  }
+}
+
+// If this file is imported as a module, it will not execute the processing function.
+if (import.meta.url === pathToFileURL(process.argv[1]).href) {
+  // Read the file path from command line arguments if provided, otherwise print help text.
+  const input = process.argv[2];
+  if (!input) {
+    console.log("Usage: node src/index.js <file-path>");
+    console.log("Please provide a file path to process robots.");
+    process.exit(1);
+  }
+
+  import("fs")
+    .then(async (fs) => {
+      try {
+        const data = await fs.promises.readFile(input, "utf8");
+        processRobots(data);
+      } catch (err) {
+        console.error(`Error reading file: ${err.message}`);
+        process.exit(1);
+      }
+    })
+    .catch((err) => {
+      console.error(`Error importing fs module: ${err.message}`);
+      process.exit(1);
+    });
+}
