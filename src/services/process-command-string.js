0df52c50f9a66908bbcee53491bd6136188ce79e
Andreas Nel <nel.andreas1@gmail.com>
2025-07-01 22:07:31 +0000

    Prevent processing of commands after robot is lost

--- src/services/process-command-string.js
+++ src/services/process-command-string.js
@@ -1,31 +1,32 @@
 import CommandFactory from "../models/commands/factory.js";
 
+class RobotLostError extends Error {}
+
 export default function processCommandString(robot, commandString, world) {
   let finalPosition = robot.toString();
-  let isRobotPositionValid = true;
-  commandString.split("").forEach((commandChar) => {
-    if (
-      !(
-        isRobotPositionValid &&
-        world.isSafeCommand(commandChar, robot.toString())
-      )
-    ) {
-      // No need to process further commands if the robot is already lost or the command is blacklisted.
-      return;
-    }
+  try {
+    commandString.split("").forEach((commandChar) => {
+      if (!world.isSafeCommand(commandChar, robot.toString())) {
+        return;
+      }
 
-    // Technically we don't need the world to be passed down.
-    // If commands were refactored to return their new desired state,
-    // we could just check the robot's position against the world after each command execution.
-    const command = CommandFactory.createCommand(commandChar, robot, world);
-    isRobotPositionValid = command.execute();
-
-    if (isRobotPositionValid) {
+      // Technically we don't need the world to be passed down.
+      // If commands were refactored to return their new desired state,
+      // we could just check the robot's position against the world after each command execution.
+      const command = CommandFactory.createCommand(commandChar, robot, world);
+      const isRobotPositionValid = command.execute();
+      if (!isRobotPositionValid) {
+        world.blacklistCommand(commandChar, command.backup);
+        // If lost, we don't need to process any further commands for this robot.
+        throw new RobotLostError(`${command.backup} LOST`);
+      }
       finalPosition = robot.toString();
-    } else {
-      world.blacklistCommand(commandChar, command.backup);
-      finalPosition = `${command.backup} LOST`;
+    });
+    return finalPosition;
+  } catch (error) {
+    if (error instanceof RobotLostError) {
+      return error.message;
     }
-  });
-  return finalPosition;
+    throw error;
+  }
 }
